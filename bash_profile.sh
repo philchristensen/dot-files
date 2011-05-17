532c4c56ab6c744e1d422c7758e3414a1aef620f
Phil Christensen <phil@bubblehouse.org>
2011-05-17 12:52:21 -0400

    local changes

--- bash_profile.sh
+++ bash_profile.sh
@@ -4,9 +4,12 @@ export EDITOR=nano
 
 export CLOJURE_JAVA=/usr/lib/jvm/java-6-sun/bin/java
 
-export WORKON_HOME=/home/phil/Workspace/.virtual
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.6
-source /usr/local/bin/virtualenvwrapper.sh
+export WORKON_HOME=$HOME/Workspace/.virtual
+#export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.6
+export VIRTUALENVWRAPPER_PYTHON=/opt/local/bin/python2.6
+
+#source /usr/local/bin/virtualenvwrapper.sh
+source /opt/local/Library/Frameworks/Python.framework/Versions/2.6/bin/virtualenvwrapper.sh
 
 # include .bashrc if it exists
 if [ -f "$HOME/.profile" ]; then
